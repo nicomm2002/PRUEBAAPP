07c84df896c9199a4cb22270576cdb4963a174fb
Nicolas Martinez <nicolascir.97@gmail.com>
2025-03-24 19:59:00 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,9 +5,6 @@
 #SBATCH --mem=1G
 #SBATCH --time=00:15:00
 
-# Cambiar al directorio de trabajo correcto
-cd "C:\Users\ASUS\OneDrive\Escritorio\Matlab brainlife\EJEMPLO"
-
 # parse config.json to load the input parameters needed for the app
 t1=$(jq -r .t1 config.json)
 
