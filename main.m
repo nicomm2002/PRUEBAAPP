8f8f710bf0b12e964b960b3c4fe8b501a72f2dda
Nicolas Martinez <nicolascir.97@gmail.com>
2025-03-24 22:40:49 +0100

    Update main.m

--- main.m
+++ main.m
@@ -10,24 +10,12 @@ function main1(t1)
 % Cargar el archivo de configuración
 config = loadjson('config.json);
 
-% Pasar las entradas y salidas leídas del archivo config.json a la función 
-% que cargará el archivo T1w, lo resampleará y escribirá el nuevo archivo 
-% resampleado a una resolución de 1 mm.
-%
-% Esto guardará en el directorio actual un archivo llamado "T1w_resliced1mm.nii.gz"
-% porque ese es el nombre del archivo almacenado en config.outname
+% Pasar las entradas y salidas leídas del archivo config.json a la función que cargará el archivo T1w, lo resampleará y escribirá el nuevo archivo resampleado a una resolución de 1 mm. % Esto guardará en el directorio actual un archivo llamado "T1w_resliced1mm.nii.gz" porque ese es el nombre del archivo almacenado en config.outname
+
 reslice_nii(config.t1, ...
             config.outname, ...
             config.outres, ...
             true, true, 2);
         
-% Uso de reslice:
-% reslice_nii(old_fn, ...
-%             new_fn, ...
-%             [voxel_size], ...
-%             [verbose], .... 
-%             [bg], ...
-%             [method], [img_idx], [preferredForm]);
-
 
  
