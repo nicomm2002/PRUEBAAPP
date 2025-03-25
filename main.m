18c90b313ef16cbed08c0b1cc8b3a4f78a468c76
Nicolas Martinez <nicolascir.97@gmail.com>
2025-03-25 19:05:35 +0100

    Update main.m

--- main.m
+++ main.m
@@ -5,9 +5,9 @@ jsonData = jsondecode(fileread('config.json.example'));
 t1 = jsonData.t1;
 
 % Convertir t1 a número si es una cadena
-if ischar(t1)
-    t1 = str2double(t1);
-end
+%if ischar(t1)
+  %  t1 = str2double(t1);
+%end
 
 % Mostrar la suma de 1 + t1
 disp(['La suma de 1 + t1 es: ', num2str(1 + 1)]);  
