2f0b68feafe8cc06932e77ad5a656754f8229620
Nicolas Martinez <nicolascir.97@gmail.com>
2025-03-25 13:46:19 +0100

    Update main.m

--- main.m
+++ main.m
@@ -2,9 +2,6 @@ function main1(t1)
     disp(['Parameter t1: ', t1]);
     % Aquí puedes poner el resto de tu código
 
-% Agregar la ruta de JSONLab
- addpath(genpath('C:\Users\ASUS\OneDrive\Escritorio\Matlab brainlife\jsonlab-2.9.8'));
- addpath(genpath('C:\Users\ASUS\OneDrive\Escritorio\Matlab brainlife\NIfTI_20140122'));
 
 % Cargar el archivo de configuración
 config = loadjson('config.json);
@@ -28,23 +25,6 @@ reslice_nii(config.t1, ...
 %             [bg], ...
 %             [method], [img_idx], [preferredForm]);
 
- % PARTE 2
-% % Especificar la ruta al archivo .nii
- niiFilePath = 'C:/Users/ASUS/OneDrive/Escritorio/Matlab brainlife/EJEMPLO/data/t1.nii';
- niiFilePath1 = 'C:/Users/ASUS/OneDrive/Escritorio/Matlab brainlife/EJEMPLO/T1w_resliced1mm.nii';
- 
-% Leer el archivo .nii
- niiData = niftiread(niiFilePath);
- niiData1 = niftiread(niiFilePath1);
-% 
-% % Mostrar información sobre los datos cargados
- disp(size(niiData));
-
- % Visualizar el primer corte
- figure(1)
- imshow(niiData(:,:,1), []);
- figure(2)
- imshow(niiData1(:,:,1), []);
  pause;
 end
  
