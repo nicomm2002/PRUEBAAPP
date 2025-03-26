91889e13ff2138d8e73fdeb82a5a22a66ad79280
Nicolas Martinez <nicolascir.97@gmail.com>
2025-03-26 13:27:57 +0100

    Update main.m

--- main.m
+++ main.m
@@ -4,11 +4,6 @@ jsonData = jsondecode(fileread('config.json'));
 % Obtener el valor de t1
 t1 = jsonData.n1;
 
-% Convertir t1 a número si es una cadena
-if ischar(t1)
-    t1 = str2double(t1);
-end
-
 suma = 1 + t1;
 
 % Mostrar la suma
