d38e9d907cb462adc969b2d05b29648eb6034edc
Nicolas Martinez <nicolascir.97@gmail.com>
2025-03-31 11:28:45 +0200

    Update main.m

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ jsonData = jsondecode(fileread('config.json'));
 % Obtener el valor de t1
 t1 = jsonData.n1;
 
-suma = 1 + t1;
+suma = 1 + n1;
 
 % Mostrar la suma
 disp(['La suma de 1 + t1 es: ',(suma)]);
